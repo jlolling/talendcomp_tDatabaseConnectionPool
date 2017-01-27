13bfd09c0b4466cd73683fcfb9e9fb0d4cefc46d
Jan Lolling <jan.lolling@gmail.com>
2017-01-27 18:55:48 +0100

    improved tests and new build

--- src/test/java/de/cimt/talendcomp/connectionpool/test/TestThreadLocalConnectionCache.java
+++ src/test/java/de/cimt/talendcomp/connectionpool/test/TestThreadLocalConnectionCache.java
@@ -3,7 +3,6 @@ package de.cimt.talendcomp.connectionpool.test;
 import static org.junit.Assert.assertTrue;
 
 import java.sql.Connection;
-import java.sql.Driver;
 import java.util.Hashtable;
 
 import javax.management.ObjectName;
@@ -21,7 +20,7 @@ public class TestThreadLocalConnectionCache {
 		String passwd = "postgres";
 		String driverClass = "org.postgresql.Driver";
 		System.out.println(Thread.currentThread().getName() + ": load driver");
-		Driver driver = (Driver) Class.forName(driverClass).newInstance();
+		Class.forName(driverClass).newInstance();
 		System.out.println(Thread.currentThread().getName() + ": connect");
 		Connection conn_tPostgresqlConnection_1 = java.sql.DriverManager
 				.getConnection(url,
@@ -33,15 +32,15 @@ public class TestThreadLocalConnectionCache {
 	@Test
 	public void testSetGet() throws Exception {
 		System.out.println(Thread.currentThread().getName() + ": start");
-		Connection conn = createConnection();
-		ThreadLocalConnectionCache.set("test", conn);
+		Connection conn1 = createConnection();
+		ThreadLocalConnectionCache.set("test", conn1);
 		Connection conn2 = ThreadLocalConnectionCache.get("test");
 		if (conn2.isClosed()) {
 			throw new IllegalStateException("Connection was closed already!");
 		}
 		conn2.close();
 		System.out.println(Thread.currentThread().getName() + ": end");
-		assertTrue(true);
+		assertTrue(conn1 == conn2);
 	}
 	
 	@Test
